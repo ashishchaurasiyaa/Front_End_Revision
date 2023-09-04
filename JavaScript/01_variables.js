67062de0dfd8b85627e0d2bd1b47481847129940
Ashish <AChaurasiya@youngman.co.in>
2023-09-04 10:43:14 +0530

    javascript revision

---
+++ JavaScript/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14553
+let accountEmail = "ashish@google.com"
+var accountPassword = "12345"
+let accountState;
+accountCity = "Kanpur"
+
+// accountId = 2 Not allowed
+// console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Noida"
+accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ JavaScript/03_conversionOperations.js
@@ -0,0 +1,52 @@
+// let score = "33"
+// let score = null
+// let score = undefined
+// let score = true
+
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33qbc" => NaN
+// true => 1
+
+// let isLoggedIn = 1
+// let isLoggedIn = ""
+let isLoggedIn = "Ashish"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+// "Ashish" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// Operations
+
+let value = 3
+
+let negValue = -value
+
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = "Ashish"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2 + 2);
+console.log(1+2 + "2");
+console.log((3+4) * 5 % 3);
+
