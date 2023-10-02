a78fa52ad770b9e4f0bd00ce33635f7c7f507ca7
Ashish <AChaurasiya@youngman.co.in>
2023-10-02 14:44:33 +0530

    comprasion

--- JavaScript/03_conversionOperations.js
+++ JavaScript/03_conversionOperations.js
@@ -47,16 +47,38 @@ let str3 = str1 + str2
 console.log(str3);
 
 console.log("1" + 2 + 2);
-console.log(1+2 + "2");
-console.log((3+4) * 5 % 3);
+console.log(1 + 2 + "2");
+console.log((3 + 4) * 5 % 3);
 
 console.log(+true)
 console.log(+"")
 let num1, num2, num3
-num1 = num2 = num3 = 2+2
+num1 = num2 = num3 = 2 + 2
 let gameCounter = 100
 gameCounter++;
 ++gameCounter;
 console.log(gameCounter)
+console.log(2 > 1)
+console.log(2 == 1)
+console.log(2 != 1)
+console.log(2 < 1)
+console.log("2" > 1)
+console.log("02" > 1)
+console.log(null >= 0)
 
+console.log(undefined == 0)
+    // Strict check check the datatype and values
+console.log(undefined === 0)
 
+console.log("2" == 2);
+console.log(undefined > 0)
+
+// Comprasion and equlity
+
+// Confusion comprasion and avoid this type code
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
