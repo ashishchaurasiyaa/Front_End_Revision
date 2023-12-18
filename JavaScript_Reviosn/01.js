d271d7df7e0e38cc9743fc787c1c8cac38afd800
Ashish <AChaurasiya@youngman.co.in>
2023-12-18 09:40:00 +0530

    javascript

--- JavaScript_Reviosn/01.js
+++ JavaScript_Reviosn/01.js
@@ -2,7 +2,7 @@
 // Foreach is a loop which works on array
 // Foreach is a HOF higher order function
 
-let a = [1, 2, '3', 4];
+// let a = [1, 2, '3', 4];
 // a.forEach(function(val) {
 //     console.log(val + 2)
 // })
@@ -57,13 +57,13 @@ let a = [1, 2, '3', 4];
 
 // sync vs async
 
-console.log("1");
-console.log("2");
-console.log("3");
-setTimeout(function() {
-    console.log("4");
-}, 5000)
-console.log("5");
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// setTimeout(function() {
+//     console.log("4");
+// }, 5000)
+// console.log("5");
 
 // synchronous code executes line by line and if first line is not resolved the code execution will not move to the next line
 
@@ -73,14 +73,82 @@ console.log("5");
 
 // jab facebook se photo aajaye to chal jaana - delay callback callback hota hai function jo ki chalta hai async code async code kuchh der baad chalta hai, kyuki wo code kuchh der baad chlta hai to humein nahi pata hota hai, ke kab chalega to jab bhi chale hum async code ko ek function dedete hai ke jab bhi chalna is function ko chala dena, wo jo function humne diya tha us function ko hum kahte hai callback
 
-setTimeout(function() {
+// setTimeout(function() {
 
-}, 1000)
+// }, 1000)
 
 // objects - access loop
 // freeze, this call apply bind
 
 // Write a JavaScript function to check whether an input is an array or not Test Data :
-function checkArray(inp) {
-    Array.isArray(inp);
+// function checkArray(inp) {
+//     Array.isArray(inp);
+// }
+
+// console.log(checkArray(1));
+// console.log(checkArray({}))
+// console.log(checkArray([1, 2, 3]))
+
+// Whenever we create an array it is always fiven a blueprint ot an instance which is Array
+
+// function checkArray(inp) {
+//     return inp instanceof Array;
+// }
+
+// console.log(checkArray([1, 2, 3, 4]));
+// console.log(checkArray(1))
+// console.log(checkArray("Ashih"))
+
+// function cloning(inp) {
+
+// }
+// cloning([1, 2, 3, 4])
+
+// function getter(inp, get = 1) {
+//     let cloned = []
+//     for (let i = 0; i < get; i++) {
+//         cloned.push(inp[i])
+//     }
+//     return cloned
+// }
+// console.log(getter([1, 2, 3, 4, 5, 6, 7, 8]))
+
+// Q4:
+// Write a simple JavaScript program to join all elements of the following array into a string. Go to the editor
+// Sample array : myColor = ["Red", "Green", "White"
+// "Black"];
+// Expected Output :
+// "Red, Green, White, Black"
+// "Red+Green+White+Black"
+
+function joiner(inp) {
+    return inp.join("+");
+
 }
+
+console.log(joiner(["apple", "banana", "guava", "cat", "piplani"]))
+
+// Q5 possible ways to create an object:
+
+let a = {} // object literal
+let ans = Object.create(null) //by using object create method
+function abcd() {
+    this.name = "Ashish";
+}
+
+let obj3 = new abcd();
+
+class Abcd {
+    constructor() {}
+    a = 12;
+}
+
+let anss = new Abcd
+console.log(anss)
+
+// Q6 what is this call apply bind
+// Q7 what is a hof
+
+// Q8 difference between var let and const
+// Q9 what is iife
+// Q10 what is hoisting
